8e51fb27822ae3a6957f439d90591e373270ab8a
Shakna Israel <s4b3r6@gmail.com>
2014-10-23 14:39:15 +1100

    Minify Assets

--- assets/js/google_search.js
+++ assets/js/google_search.js
@@ -1,10 +1,1 @@
-  (function() {
-    var cx = '015761949155185830479:75tl7usajj8';
-    var gcse = document.createElement('script');
-    gcse.type = 'text/javascript';
-    gcse.async = true;
-    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-        '//www.google.com/cse/cse.js?cx=' + cx;
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(gcse, s);
-  })();
+(function(){var e="015761949155185830479:75tl7usajj8";var t=document.createElement("script");t.type="text/javascript";t.async=true;t.src=(document.location.protocol=="https:"?"https:":"http:")+"//www.google.com/cse/cse.js?cx="+e;var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n)})()
